b8a56de0edcc21628bdb3870d523042bd1c064e1
dineshkumar.a <dineshkumar.a@y3inpc-bkxqgc2.ych.com>
2020-10-28 18:30:18 +0800

    Initial commit

---
+++ src/main/java/com/dreamJackpot/DreamJackpotApplication.java
@@ -0,0 +1,13 @@
+package com.dreamJackpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamJackpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamJackpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamJackpot/DreamJackpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamJackpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamJackpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
